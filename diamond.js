867e090b4dc348a3a68d2c9a1ef2dd185176266e
JuliusS99 <julius.schirmer99@gmail.com>
2020-05-25 23:07:10 +0200

    Test für äußere und innere Striche

--- diamond.js
+++ diamond.js
@@ -11,10 +11,24 @@ const innerSlots = (currentLetter) => {
         innerDashes += '-';
     };
     return innerDashes;
+};
+
+const outerSlots = (targetLetter, currentLetter) => {
+    const innerSlotsTargetLetter = alphabet.indexOf(targetLetter) *2 - 1 ; // C: 3
+    let innerDashesCurrentLetter = alphabet.indexOf(currentLetter) *2 - 1; // B: 1, A:0
+    amountOfDashes = (innerSlotsTargetLetter - innerDashesCurrentLetter) /2; //B: 1 A:?
+
+    let outerDashes = '';
+    for(let i = 0; i < amountOfDashes; i++){
+        outerDashes += '-';
+    };
+
+    return outerDashes;
 
 };
 
 module.exports = {
     diamond,
-    innerSlots
+    innerSlots,
+    outerSlots
 };
--- diamond.test.js
+++ diamond.test.js
@@ -20,3 +20,14 @@ describe("innere Striche", () => {
         })
     })
 });
+
+describe("äußere Striche", () => {
+    [
+        {targetLetter: 'C', currentLetter: 'A', output: '--'},
+        {targetLetter: 'C', currentLetter: 'B', output: '-'}
+    ].forEach(({targetLetter, currentLetter, output}) => {
+        it(`should return ${output} on input ${targetLetter} and ${currentLetter}`, () => {
+            expect(diamond.outerSlots(targetLetter, currentLetter)).toEqual(output)
+        })
+    })
+});
